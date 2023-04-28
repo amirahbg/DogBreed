80b3acd068651d504cc3096a3a6d60bde5613b3d
amir.bagheri <amirahbg@gmail.com>
2023-04-28 09:50:21 +0430

    add {breedName}/images path to DogBreedService.kt

--- app/src/main/java/com/example/dogbreed/data/remote/DogBreedService.kt
+++ app/src/main/java/com/example/dogbreed/data/remote/DogBreedService.kt
@@ -1,9 +1,14 @@
 package com.example.dogbreed.data.remote
 
 import com.example.dogbreed.data.remote.model.AllBreedsResponse
+import com.example.dogbreed.data.remote.model.BreedImageResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DogBreedService {
     @GET("list/all")
     suspend fun getAllBreeds(): AllBreedsResponse
+
+    @GET("{breedName}/images")
+    suspend fun getBreedImages(@Path("breedName") breedName: String): BreedImageResponse
 }
